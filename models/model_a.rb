753a149192e240e2810ba9b2ec14ac9fe20539d9
Wade Naughton <wadenaughton@gmail.com>
2021-09-24 12:07:05 -0400

    create prehistoric_best hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+prehistoric_beast = {
+  "species" => 'tyrannosaurus',
+  "location" => 'Europe',
+  "number_of_kin" => 2,
+  "number_of_limbs" => 4,
+  "is_alive" => false,
+  "ate_meat"  => true,
+  "habital_areas" => ['Jungle', 'Savanna', 'Marsh' ],
+  "modern_relatives" => ['Chicken', 'Emu', 'Ostrich']
+}
