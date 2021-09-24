308fdab828cecd7c9abb070b87216eb71b830be8
Wade Naughton <wadenaughton@gmail.com>
2021-09-24 12:16:05 -0400

    add developer_traits hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer_traits = {
+  "name" => 'Wade',
+  "location" => 'Maryland',
+  "age" => 30,
+  "number_of_pets" => 1,
+  "has_completed_program" => false,
+  "passed_mod0" => true,
+  "favorite_sports" => ['Soccer', 'Basketball', 'Soccer again' ],
+  "areas_lived" => ['Delaware', 'DC', 'Maryland' ]
+}
