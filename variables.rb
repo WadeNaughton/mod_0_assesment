024d1145506e893799107683e8ab2cf2c92728ce
Wade Naughton <wadenaughton@gmail.com>
2021-09-24 11:55:23 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,16 @@
+movie = "Hot Rod"
+
+is_entertaining = true
+
+movie = "Zoolander"
+
+num_4 = 4
+num_6 = 6
+
+product = num_4 * num_6
+
+favorite_foods = ["Pizza", "Salad", "Bread"]
+
+puts favorite_foods.shift
+
+puts movie.size
